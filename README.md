659528dc37d3fedab00066acf3100ab5e70a00bf
Lilly Piri <lilly@lillypiri.com>
2017-08-10 14:55:32 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Gets data from Cronitor's API to use with the custom monitoring widget on Geckoboard.
+
+
+To use on Heroku, TOKEN should be set as a Heroku environment variable (find it in settings).
+
+Node & Express.
--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-const Express = require('express');
+const Express = require("express");
 const app = Express();
 
-const CronitorClient = require('cronitor-client');
-
+const CronitorClient = require("cronitor-client");
 
 const cc = new CronitorClient({ access_token: process.env.TOKEN });
 
-app.get('/', function(req, res) {
-    cc.all(function(err, result) {
-        console.log('ERROR', err);
-        console.log('Result', result);
+app.get("/", function(req, res) {
+  cc.all(function(err, result) {
+    console.log("ERROR", err);
+    console.log("Result", result);
 
-        const failingCrons = result.monitors.filter(m => {
-            // if a check is not running and not passing then it is borked
-            return m.running == false && m.passing == false;
-        });
-
-        // All good if there are no failing crons
-        const status = failingCrons.length === 0 ? 'Up' : 'Down';
-
-        // Grab the first failing cron for the down time
-        const downTime =
-            failingCrons.length === 0 ? '' : failingCrons[0].status;
-
-
-        /* Formatted response from API this needs to follow Geckoboard's formatting outlined at http://developer-custom.geckoboard.com/#monitoring */
-        res.json({
-            status,
-            downTime,
-            responseTime: ''
-        });
+    const failingCrons = result.monitors.filter(m => {
+      // if a check is not running and not passing then it is borked
+      return m.running == false && m.passing == false;
     });
+
+    // All good if there are no failing crons
+    const status = failingCrons.length === 0 ? "Up" : "Down";
+
+    // Grab the first failing cron for the down time
+    const downTime = failingCrons.length === 0 ? "" : failingCrons[0].status;
+
+    /* Formatted response from API this needs to follow Geckoboard's formatting outlined at http://developer-custom.geckoboard.com/#monitoring */
+    res.json({
+      status,
+      downTime,
+      responseTime: ""
+    });
+  });
 });
 
 // http://localhost:3000/
-app.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000!'));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Listening on port 5000!")
+);
